b79ec8dc2f5ad186c3367d3da12899b0f73f588d
musale <musale@focusmobile.co>
2017-06-15 12:19:38 +0300

    Add handlers to fetch sources and articles.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from '../components/Header';
 import LHSMenu from '../components/LHSMenu';
+import getSources from '../utils/getSources';
+import getArticles from '../utils/getArticles';
+import HandleArticle from './SourceArticle';
 import '../css/App.css';
-const items = [
-  {name: 'Jeff'},
-  {name: 'Koinange'},
-  {name: 'Keff'},
-  {name: 'Joinange'},
-  {name: 'Jay'},
-]
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      articles: [],
+      source: ''
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount() {
+    getSources().then((response) => {
+      this.setState({items: response.data.sources})
+    });
+  }
+  handleClick(source, e){
+    e.preventDefault();
+    getArticles(source.id).then((response) => {
+      this.setState({articles: response.data.articles, source: source.name})
+    });
+  }
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header/>
         <div className="container-fluid row">
-          <div className="col-md-3">
-            <LHSMenu sources={items} />
+          <div className="col-md-2">
+            <LHSMenu sources={this.state.items} handleClick={this.handleClick}/>
           </div>
-          <div className="col-md-9">Articles</div>
+          <div className="col-md-9" data-spy="affix" data-offset-top="97">
+            <HandleArticle articles={this.state.articles} source={this.state.source}/>
+          </div>
         </div>
       </div>
     );
