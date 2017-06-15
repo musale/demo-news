253b4cc15e39f676f7190220f4e1aed8551e285e
musale <musale@focusmobile.co>
2017-06-15 12:21:17 +0300

    Add a route to sources

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
 import App from '../containers/App';
+import SourceArticle from '../containers/SourceArticle';
 import NotFound from '../containers/404';
 
 const Routes = () => (
   <Switch>
     <Route path="/" component={App} />
+    <Route path="/:source" component={SourceArticle} />
     <Route path="*" component={NotFound} />
   </Switch>
 );
