620c3203d77c2b14276e44334698882479c6d9a1
musale <musale@focusmobile.co>
2017-06-15 12:53:43 +0300

    Add shoutout to News API.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-toggleable-md navbar-light fixed-top bg-faded">
+    <nav className="navbar navbar-toggleable-md navbar-light fixed-top bg-faded ml-auto">
       <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <a className="navbar-brand" href="/home">News App</a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <span class="navbar-text ">
+          Interfacing with <i><a href="https://newsapi.org" title="News API">News API</a></i>
+        </span>
+      </div>
     </nav>
   );
 }
